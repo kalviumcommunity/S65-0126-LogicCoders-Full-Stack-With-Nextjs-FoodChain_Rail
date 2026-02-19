89254bad4d3612acf8aa636cd6d7c9aa977fc9e7
Ankitkr000 <ankitkr1001a@gmail.com>
2026-02-19 23:56:03 +0530

    backend done

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
 
-// Health check
+
 app.get("/", (req, res) => {
     res.json({
         status: "success",
